ef224f9410db0eff0002739ecb303f7c1a0338dd
JoyMarocho <joymarocho@gmail.com>
2022-05-08 14:26:23 +0300

    debug

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bigbaby:New Password@localhost/Watchlist-master'
 
-
+@staticmethod
+def init_app(app):
+        pass
 
 class ProdConfig(Config):
     '''
@@ -24,9 +31,10 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    DEBUG = True
-class Config:
-    '''
-    General configuration parent class
-    '''
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}
