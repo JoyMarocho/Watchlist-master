62cabf8226d910722c315fa73f68206de15ecfb9
JoyMarocho <joymarocho@gmail.com>
2022-05-24 20:38:40 +0300

    commits

--- app/models.py
+++ app/models.py
@@ -1,9 +1,28 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+def __repr__(self):
+        return f'User {self.username}'
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
+if __name__ == '__main__':
+    manager.run()
+
+
+
 class Movie:
     '''
     Movie class to define Movie Objects
     '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+def __init__(self,id,title,overview,poster,vote_average,vote_count):
         self.id =id
         self.title = title
         self.overview = overview
@@ -17,23 +36,23 @@ class Review:
 
     all_reviews = []
 
-    def __init__(self,movie_id,title,imageurl,review):
+def __init__(self,movie_id,title,imageurl,review):
         self.movie_id = movie_id
         self.title = title
         self.imageurl = imageurl
         self.review = review
 
 
-    def save_review(self):
+def save_review(self):
         Review.all_reviews.append(self)
 
 
-    @classmethod
-    def clear_reviews(cls):
+@classmethod
+def clear_reviews(cls):
         Review.all_reviews.clear()
 
-    @classmethod
-    def get_reviews(cls,id):
+@classmethod
+def get_reviews(cls,id):
 
         response = []
 
